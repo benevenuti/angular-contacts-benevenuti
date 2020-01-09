35ade05ef3a8e107f04fcf591759af6869f63454
Filipe Benevenuti <filipe.benevenuti@farmaciassaojoao.com.br>
2020-01-09 15:12:53 -0300

    Finalizei layout em table e card

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -16,26 +16,19 @@ import { MatSort, MatTableDataSource, MatTable } from "@angular/material";
   styleUrls: ["./list.component.scss"]
 })
 export class ListComponent implements OnInit {
-  displayedColumns: string[] = ["id", "name", "email", "tel"];
+  displayedColumns: string[] = ["id", "name", "email", "tel", "actions"];
 
   @Input() contacts: Contact[];
   @Output() editContactEvent: EventEmitter<Contact> = new EventEmitter();
   @Output() deleteContactEvent: EventEmitter<Contact> = new EventEmitter();
 
   dataSource = new MatTableDataSource(this.contacts);
-  // @ViewChild(MatTable, { static: false }) table: MatTable<any>;
 
   @ViewChild(MatSort, { static: false }) sort: MatSort;
   ngOnInit(): void {
     this.dataSource.data = this.contacts;
     this.dataSource.sort = this.sort;
   }
-
-  // ngOnChanges(changes: SimpleChanges) {
-  //   this.dataSource.data = this.contacts;
-  //   console.info(this.contacts);
-  //   this.table.renderRows();
-  // }
 
   doEdit(contact: Contact): void {
     this.editContactEvent.emit(contact);
